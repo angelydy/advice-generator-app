19a4848e7de24b118ce944e8c033ad1ca3e7165c
angely <angelymaedyy@gmail.com>
2022-06-30 22:49:13 +0800

    adds javascript

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 var adviceId = document.getElementById("adviceId")
 var adviceQuote = document.getElementById("adviceQuote")
+var randomBtn = document.querySelector(".randomBtn")
+var advice;
+
+function onLoadAdvice() {
+  fetch('https://api.adviceslip.com/advice', {cache: 'no-store'}).then(function(response) {
+    return response.json();
+  }).then(function (adviceData) {
+    advice = adviceData.slip
+    adviceQuote.innerHTML = advice.advice;
+    adviceId.innerHTML = "Advice #" + String(advice.id);
+  })['catch'](function (error) {
+    console.log(error);
+  });
+}
+randomBtn.addEventListener('click', onLoadAdvice);
+
+window.onload = function() {
+  onLoadAdvice();
+};
